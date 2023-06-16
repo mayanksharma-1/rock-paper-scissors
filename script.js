6f63b907b9ef80a02725f5d101ae47895e3a195e
Mayank Sharma <as.mayanksharma@gmail.com>
2023-06-16 21:56:26 +0530

    create function to get player choice

--- script.js
+++ script.js
@@ -2,5 +2,14 @@ function getComputerChoice(){
     const choices = ['rock','paper','scissors']
     const randomChoice = choices[Math.floor(Math.random()*3)]
     console.log("computer chose " + randomChoice)
+    return randomChoice
 }
+function getPlayerChoice(){
+    const choice = prompt('Please enter your choice Rock, Paper, or Scissors')
+    console.log("player chose " + choice)
+    
+    return choice
+}
+
 getComputerChoice()
+getPlayerChoice()
