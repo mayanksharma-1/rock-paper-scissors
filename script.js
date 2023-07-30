68d4b177092a2bf7661678fee29f37b1a6d696b5
Mayank Sharma <as.mayanksharma@gmail.com>
2023-07-30 11:49:31 +0530

    setup buttons and show the game output on html

--- script.js
+++ script.js
@@ -4,30 +4,6 @@ function getComputerChoice(){
     console.log("computer chose " + randomChoice);
     return randomChoice;
 }
-function getPlayerChoice(){
-    let choice = prompt('Please enter your choice Rock, Paper, or Scissors');
-    const choices = ['rock','paper','scissors'];
-    choice = makeLowercase(choice);
-    if (choice == choices[0] | choice == choices[1] | choice == choices[2]){
-    // if (choice in choices){
-
-        console.log("player chose " + choice);
-        return choice;
-    }
-    else{
-        console.log('Invalid option Error')
-        return -1;
-    }
-}
-function makeLowercase(string) {
-    let lowercaseString = "";
-  
-    for (let character of string) {
-      let lowercaseCharacter = character.toLowerCase();
-      lowercaseString += lowercaseCharacter;
-    }
-    return lowercaseString;
-  }
 
 function playRound(playerChoice,computerChoice) {
     if (playerChoice==-1){
@@ -60,36 +36,15 @@ function playRound(playerChoice,computerChoice) {
 }
 
 function game(){
-    let playerScore = 0;
-    let computerScore = 0;
-    let drawScore = 0;
-    for (let i =0;i<5;i++){
-        let playerChoice = getPlayerChoice();
-        let computerChoice = getComputerChoice();
-        console.log(playRound(playerChoice,computerChoice));
-        if (playRound(playerChoice,computerChoice) == "player wins"){
-            playerScore += 1;
-        }
-        else if(playRound(playerChoice,computerChoice) == "computer wins"){
-            computerScore += 1;
-        }
-        else if(playRound(playerChoice,computerChoice) == "It's a Draw "){
-            drawScore +=1;
-        }
-        else{
-            console.log('Try again round not counted because of error');
-            i -= 1;
-        }
+        const buttons = document.querySelectorAll(".button");
+        let playerChoice = '';
+        buttons.forEach((button)=>{
+            button.addEventListener('click',event=>{playerChoice = event.target.id;
+                let computerChoice = getComputerChoice();     
+                answerDiv = document.querySelector('.answer');
+                answerDiv.innerHTML = playRound(playerChoice,computerChoice);
+            });
+        });
     }
-    if (playerScore>computerScore){
-        console.log('Player wins the best of five');
-    }
-    else if(computerScore>playerScore){
-        console.log('Computer wins the best of five');
-    }
-    else if(playerScore==computerScore){
-        console.log('The best of five is a Draw')
-    }
-}
 
 game();
